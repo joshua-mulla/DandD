5964945b714a31494ec94404112d37fcdc0fffc8
Josh Mulla <josh.mulla1@gmail.com>
2016-02-12 18:59:07 -0700

    refactored code and deleted extranneous files

--- exports.js
+++
@@ -1,4 +0,0 @@
-var sorter = require('./sort.js');
-var array =[1,2,7,4,8];
-sorter.rmDuplicates(array);
-sorter.smallToBig(array);
--- foo.js
+++
@@ -1,9 +0,0 @@
-/*var myObject = require('./obj.js');
-var search = require('./search.js')
-
-var foo = search.find('wrathful smite',myObject);
-console.log(foo);*/
-
-var object = [1,2,3,4];
-console.log(typeof(object));
-console.log(typeof(object[1]));
